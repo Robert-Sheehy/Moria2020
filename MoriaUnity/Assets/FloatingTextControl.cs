b81d16cfe5b6c3c8550ff3a2aa1ff08e28b85db2
Ian P. Sweeney <Ian.P.Sweeney@students.ittralee.ie>
2019-09-24 15:49:59 +0100

    Worked on scripts and objects to allow use of Floating Text

---
+++ MoriaUnity/Assets/FloatingTextControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class FloatingTextControl : MonoBehaviour
+{
+    TMP_Text bubbleText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bubbleText = GetComponentInChildren<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    internal void setMessage(string v)
+    {
+        bubbleText.text = v;
+    }
+
+    internal void copyParentName()
+    {
+        bubbleText.text = name;
+    }
+
+    internal void setParent(Transform transformOfParent)
+    {
+        transform.parent = transformOfParent;
+    }
+
+    internal void setColor()
+    {
+        Color textColor = new Color(1.5f, 0f, 1.5f);
+        bubbleText.color = textColor;
+    }
+
+    internal void increaseSize()
+    {
+        bubbleText.fontSize += 1;
+    }
+
+    internal void decreaseSize()
+    {
+        bubbleText.fontSize -= 1;
+    }
+}
---
+++ MoriaUnity/Assets/TestFloatingText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFloatingText : MonoBehaviour
+{
+    public Transform FloatingTextClonable;
+
+    FloatingTextControl myBubble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBubble = FindObjectOfType<FloatingTextControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))  Instantiate(FloatingTextClonable);
+
+        if (Input.GetKeyDown(KeyCode.A)) myBubble.setMessage("Moria2020");
+        if (Input.GetKeyDown(KeyCode.S)) myBubble.copyParentName();
+        if (Input.GetKeyDown(KeyCode.B)) myBubble.setParent(transform);
+        if (Input.GetKeyDown(KeyCode.C)) myBubble.setColor();
+        if (Input.GetKeyDown(KeyCode.UpArrow)) myBubble.increaseSize();
+        if (Input.GetKeyDown(KeyCode.DownArrow)) myBubble.decreaseSize();
+    }
+}
