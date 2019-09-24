e9a7a453430075b41b1b3365e2cf83bf6f0d273b
Benjamin M. Lenihan <Benjamin.M.Lenihan@students.ittralee.ie>
2019-09-24 15:57:05 +0100

    Signed-off-by: Benjamin M. Lenihan <Benjamin.M.Lenihan@students.ittralee.ie>

---
+++ MoriaUnity/Assets/Character_Stats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Stats : MonoBehaviour
+{
+    int maxHealth = 100;
+    int minHealth = 0;
+    int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    internal void addHealth(int v)
+    {
+        
+        currentHealth += v;
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+
+    }
+
+    internal void removeHealth(int v)
+    {
+
+        currentHealth -= v;
+        if (currentHealth <= minHealth)
+        {
+            currentHealth = minHealth;
+            print("Your are dead gg mate");
+        }
+    }
+
+    internal int getHealth()
+    {
+        return currentHealth;
+
+    }
+}
---
+++ MoriaUnity/Assets/TestCharacterStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacterStats : MonoBehaviour
+{
+    Character_Stats myTestGuy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTestGuy = FindObjectOfType<Character_Stats>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) myTestGuy.addHealth(5);
+
+        if (Input.GetKeyDown(KeyCode.D)) myTestGuy.removeHealth(5);
+
+        print(myTestGuy.getHealth());
+    }
+}
