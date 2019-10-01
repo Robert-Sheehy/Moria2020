4dc150a44c278b98a027fe1332d208d5ef69cbe1
Benas <zymantas.benas@gmail.com>
2019-10-01 14:26:17 +0100

    inventory system with testing
    Signed-off-by: Benas <zymantas.benas@gmail.com>

---
+++ MoriaUnity/Assets/Scripts/Items&Text/toggleRender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleRender : MonoBehaviour
+{
+    public void toggleVisibility()
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+
+        if(rend.enabled)
+        {
+            rend.enabled = false;
+        }
+        else
+        {
+            rend.enabled = true;
+        }
+    }
+}
---
+++ MoriaUnity/Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    GameManagerScript theManager;
+    void Start()
+    {
+        theManager = FindObjectOfType<GameManagerScript>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            direction = new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            direction = new Vector3(0, 0, -1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            direction = new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            direction = new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            direction = new Vector3(-1, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            direction = new Vector3(1, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            direction = new Vector3(-1, 0, -1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            direction = new Vector3(1, 0, -1);
+        }
+
+        if (direction.magnitude > 0)
+        {
+            Vector3 newPosition = transform.position + direction;
+            if (theManager.canMoveTo(newPosition))
+            {
+                transform.position += direction;
+                transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
+    }
+}
