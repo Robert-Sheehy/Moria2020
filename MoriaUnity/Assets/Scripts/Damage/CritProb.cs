3c070fe0fa05a170d376843e4a641affb8e142ad
Benas <zymantas.benas@gmail.com>
2019-10-02 16:05:11 +0100

    Signed-off-by: Benas <zymantas.benas@gmail.com>

---
+++ MoriaUnity/Assets/Scripts/Damage/CritProb.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class CritProb : MonoBehaviour
+{
+    bool canCrit = false, canGetHitBonus = true;
+    //need script from other cs file for these stats
+    int fightingAbility = 0,
+        hitBonusOnItem = 0,
+        hitBonusForDEX = 0,
+        hitBonusForSTR = 0,
+        monsterAC = 0,
+        weightOfWeapon = 0,
+        /////////////
+        fightingAbilityAt0thLevel; //wtf ????
+        ////////////
+
+    //the below var are to be kept
+    int hitBonusOnWeapon = 0,
+        hitBonus = 0,
+        chanceToCrit = 0;
+    
+    public void Start()
+    {
+        //calculates the chance to crit
+        chanceToCrit = (((((((hitBonusOnWeapon +
+                        hitBonusOnItem) +
+                        hitBonusForDEX) +
+                        hitBonusForSTR) *
+                        5) +
+                        fightingAbility) +
+                        (weightOfWeapon * 10)) -
+                        fightingAbilityAt0thLevel);
+
+        //can it crit ?
+        if (chanceToCrit < 1 && chanceToCrit > 5000)
+        {
+            canCrit = true;
+        }
+
+        //claculates hit bonus
+        hitBonus = ((((((hitBonusOnWeapon +
+                         hitBonusOnItem) +
+                         hitBonusForDEX) +
+                         hitBonusForSTR)) *
+                         3) +
+                         fightingAbility);
+
+        //is the bonus hit number bigger then the monster AC
+        if (hitBonus<1 && hitBonus < monsterAC)
+        {
+            canGetHitBonus = true;
+        }
+        else
+        {
+            canGetHitBonus = false;
+        }
+    }
+}
---
+++ MoriaUnity/Assets/Scripts/Damage/totalDamageBonus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class totalDamageBonus : MonoBehaviour
+{
+    //kept in here
+    public int ToDamBonusOnWeapon = 0, ToDamBonusOnItem = 0, ToDamBonusFromSTR = 0;
+    //imports from other cs files
+    CritProb hitBonus;
+    Character_Stats cStrenght;
+
+    //imported from item database
+    int weaponDamage = 0, itemDamage = 0;
+
+    //calculates the bonus damage to apply
+    void Start()
+    {
+        ToDamBonusOnWeapon = weaponDamage + hitBonus;
+        ToDamBonusOnItem = itemDamage + hitBonus;
+        ToDamBonusFromSTR = cStrenght + hitBonus;
+    }
+}
