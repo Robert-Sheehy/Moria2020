2700d5866af9e91699424128d065d4b45f940b86
Mateusz Halada <mateuszhalada@gmail.com>
2019-10-15 14:24:42 +0100

    Signed-off-by: Mateusz Halada <mateuszhalada@gmail.com>

---
+++ MoriaUnity/Assets/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    List<Item> inv_List;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inv_List = new List<Item>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void AddItem(Item item)
+    {
+        inv_List.Add(item);
+        Debug.Log("inventory has " + inv_List.Count.ToString());
+    }
+    internal void RemoveItemAtIndex(int index)
+    {
+        inv_List.RemoveAt(index);
+        Debug.Log("inventory has " + inv_List.Count.ToString());
+    }
+}
---
+++ MoriaUnity/Assets/TestInvScipt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInvScipt : MonoBehaviour
+{
+    Inventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) inventory.AddItem(new Item("Dagger", "1d4"));
+        if (Input.GetKeyDown(KeyCode.Z)) inventory.RemoveItemAtIndex(3);
+    }
+}
