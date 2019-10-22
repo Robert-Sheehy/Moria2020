4e94ef40b890a7a57f77bbb3593525a10b149def
Keelan O Connor <Keelan.OConnor@students.ittralee.ie>
2019-10-22 14:33:03 +0100

    Signed-off-by: Keelan O Connor <Keelan.OConnor@students.ittralee.ie>

---
+++ MoriaUnity/Assets/Scripts/TestUIText.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//UITextCibtrol is attatched to EMPTY OBJECT called UITEXT
+//Text mesh pro is parented to UITEXT
+//TestUIText is attatched to the text mesh pro object in scene
+
+public class TestUIText : MonoBehaviour
+{
+    UnityEngine.Object UITextClone;
+    UITextControl theTextBubble;
+    // Start is called before the first frame update
+
+    float fixedRotation = 5;
+    private int MaxNUmberOfTries = 5;
+    int actualPx = 0;
+    int actualPy = 0;
+    //Vector2 r = Camera.main.ScreenPointToRay();
+    //int x = Screen.width / 2;
+    //int y = Screen.width / 2;
+
+
+    void Start()
+    {
+
+        UITextClone = Resources.Load("UIText");
+        theTextBubble = FindObjectOfType<UITextControl>();
+    }
+
+    private IEnumerator coroutine;
+   IEnumerator delayedAccess(UITextControl theText, string Message)
+    {
+        for (int i = 0; i<MaxNUmberOfTries;i++)
+        {
+            if (i>0)
+            {
+                theText.setText(Message);
+                i = MaxNUmberOfTries;
+            }
+
+            yield return null;
+        }
+    }
+
+
+    //SCREEN POSITION
+    //(rx,ry) = (actualPx/Screen.width , actualPy/Screen.height)
+    //Point (a,b) = ((a*Screen.width),(b*ScreenHeight))
+    //r = camera.main.ScreenPointToRay( )
+
+    // Ray ray = Camera.main.ScreenPointToRay(new Vector3(200, 200, 0));        
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        UITextControl TextGO;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {//Create this method*********************************************
+            TextGO = TransformTextGo("sfjkdygsdeufg", new Vector3(0.5f, 0.1f,1.0f));
+        }
+
+
+ 
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //  r = Camera.main.ScreenPointToRay();
+            // position = r.distance();
+            //Debug.Log("You clicked");
+            Vector3 mousePos = Input.mousePosition;
+            Debug.Log(mousePos.x);
+            Debug.Log(mousePos.y);
+        }
+
+    }//End update
+
+
+    private UITextControl TransformTextGo(string message, Vector3 relPosition)
+    { 
+            //Vector3 pos = default;
+            //Quaternion rot = defaullt;
+            //transform.SetPositionAndRotation(pos, rot);
+
+            GameObject TextGo =(GameObject) Instantiate(UITextClone);
+            
+            //Transform TextGO = Instantiate(textPrefab);
+            UITextControl text = TextGo.GetComponent<UITextControl>();
+            coroutine = delayedAccess(text, message);
+            StartCoroutine(coroutine);
+
+            text.positionTextAt(new Vector2(relPosition.x, relPosition.y), relPosition.z);
+
+            return text;
+        
+    }
+
+}
---
+++ MoriaUnity/Assets/Scripts/UITextControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+//This is attatched to EMPTY OBJECT called UITEXT
+//Text mesh pro is parented to UITEXT
+//TestUIText is attatched to the text mesh pro object in scene
+
+//x-1 y-0.5 z1
+//Use localpositions to define where UI elements go
+public class UITextControl : MonoBehaviour
+{
+    TMP_Text theTextObject;
+   // private TextMesh health = new TextMesh();
+    GameObject uiText;
+    //int formatIncrement = 5;
+
+    void Start()
+    {
+        theTextObject = GetComponentInChildren<TMP_Text>();
+
+        //Set position of UI
+       // theTextObject.transform.localPosition = new Vector3(-11,6);
+    } 
+
+  
+    
+
+    //NOT WORKING
+    public void changeTextSize() { 
+        //GetComponent<TextMesh>().fontSize = theTextObject.fontSize + formatIncrement;
+    }
+
+    public void transformUI() {
+        transform.parent = Camera.main.transform;
+      
+    }//End transformUI
+
+
+    internal void setText(string v)
+    {
+        theTextObject.text = v;
+    }//End setText
+
+    void Update()
+    {
+        //transform.rotation = Quaternion.LookRotation(-(Camera.main.transform.position - transform.position).normalized);
+     
+    }
+
+
+    public int calculatePosX(float actualPx)
+    {
+
+
+        return (int)((Screen.width - 1) * actualPx);
+    }
+    public int calculatePosY(float actualPy)
+    {
+        return (int)((Screen.height - 1) * actualPy);
+    }
+
+    public void positionTextAt(Vector2 relative, float dist)
+    {
+        Vector2 screenPixelPosition = new Vector2(calculatePosX(relative.x), calculatePosY(relative.y));
+
+        transform.position = Camera.main.ScreenPointToRay(screenPixelPosition).GetPoint(dist);
+
+
+    }
+}
