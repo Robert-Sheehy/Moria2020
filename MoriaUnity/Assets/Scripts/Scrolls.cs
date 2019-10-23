f3e5f34434a1a903072e90a891a212f41068addf
Benjamin M. Lenihan <Benjamin.M.Lenihan@students.ittralee.ie>
2019-10-23 12:54:48 +0100

    Signed-off-by: Benjamin M. Lenihan <Benjamin.M.Lenihan@students.ittralee.ie>

---
+++ MoriaUnity/Assets/Scripts/Scrolls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolls : Item
+{
+
+    internal Scrolls(string Name, string ModelName, int Level, int Cost)
+    {
+        itemName = Name;
+        ModelFilename = ModelName;
+        min_level = Level;
+        itemCost = Cost;
+
+    }
+}
