10f93b47e069aa5de2059ff5e86908e34868b4cf
Rob on Laptop <robert.sheehy@gmail.com>
2019-11-26 13:25:39 +0000

    Signed-off-by: Rob on Laptop <robert.sheehy@gmail.com>

--- MoriaUnity/Assets/BoardManager.cs
+++ MoriaUnity/Assets/BoardManager.cs
@@ -10,7 +10,7 @@ public class BoardManager : MonoBehaviour
     public class SubDungeon
     {
         public SubDungeon left, right;
-        public Rect rect ;
+        public Rect rect;
         public Rect room = new Rect(-1, -1, 0, 0);
         public int debugId;
 
@@ -23,21 +23,22 @@ public class BoardManager : MonoBehaviour
             debugCounter++;
         }
 
-        public bool IAmLeaf ()
+        public bool IAmLeaf()
         {
             return left == null && right == null;
         }
 
         public bool split(int minRoomSize, int maxRoomSize)
         {
-            if(!IAmLeaf())
+            if (!IAmLeaf())
             {
                 return false;
             }
-        
 
-        bool splitH;
-            if (rect.width / rect.height >= 1.25) {
+
+            bool splitH;
+            if (rect.width / rect.height >= 1.25)
+            {
                 splitH = false;
             }
             else if (rect.height / rect.width >= 1.25)
@@ -49,7 +50,7 @@ public class BoardManager : MonoBehaviour
                 splitH = Random.Range(0.0f, 1.0f) > 0.5;
             }
 
-            if(Mathf.Min(rect.height, rect.width) / 2 < minRoomSize)
+            if (Mathf.Min(rect.height, rect.width) / 2 < minRoomSize)
             {
                 Debug.Log("Sub-dungeon" + debugId + " will be a leaf");
                 return false;
@@ -57,7 +58,7 @@ public class BoardManager : MonoBehaviour
 
             if (splitH)
             {
-                int split = Random.Ramge(minRoomSize, (int)(rect.width - minRoomSize));
+                int split = Random.Range(minRoomSize, (int)(rect.width - minRoomSize));
 
                 left = new SubDungeon(new Rect(rect.x, rect.y, rect.width, split));
                 right = new SubDungeon(new Rect(rect.x, rect.y, rect.height, split));
@@ -68,15 +69,17 @@ public class BoardManager : MonoBehaviour
                 int split = Random.Range(minRoomSize, (int)(rect.height - minRoomSize));
 
             }
-            }
-    void Start()
-    {
-        
-    }
+            return true;
+        }
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
